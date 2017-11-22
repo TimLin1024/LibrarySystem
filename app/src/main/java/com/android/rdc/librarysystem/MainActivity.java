08adced2bfce75e2781aa76656871296b498525b
ivanljt <596476418@qq.com>
2017-11-22 16:58:39 +0800

    Initial commit

---
+++ app/src/main/java/com/android/rdc/librarysystem/MainActivity.java
@@ -0,0 +1,13 @@
+package com.android.rdc.librarysystem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
